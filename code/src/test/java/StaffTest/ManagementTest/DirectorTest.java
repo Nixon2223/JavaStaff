9813dbd0bfe7bf3af128630b0d23529dfb783af4
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-09 14:57:39 +0000

    director tests passed

--- code/src/test/java/StaffTest/ManagementTest/DirectorTest.java
+++ code/src/test/java/StaffTest/ManagementTest/DirectorTest.java
@@ -1,6 +1,27 @@
 package StaffTest.ManagementTest;
 
+import Staff.Managment.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DirectorTest {
 
-    Director director
+    Director director;
+
+    @Before
+    public void before(){
+        director = new Director("Jeff","124BN23", 80000.00, "Tech", 1000000.00);
+    }
+
+    @Test
+    public void hasBudget(){
+        assertEquals(1000000.00, director.getBudget(), 0.00);
+    }
+
+    @Test
+    public void hasDeptName(){
+        assertEquals("Tech", director.getDeptName());
+    }
 }
